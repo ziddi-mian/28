680e3efd14a53dc9108850128ae1c48043383114
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-28 21:20:44 +0500

    app js
    react app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,78 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="group1">
+    <div class="gr1logli">
+      <img class="group1logo " src="/Logo.png" alt="">
+
+      <div class="group1li">
+        <li id="">Home</li>
+        <li>Product</li>
+        <li>Promo</li>
+        <li>About</li>
+        <li>Contact</li>
+
+      </div>
     </div>
+    <div class="group1text">
+      <p id="g1heading"> Get Cash Back <br> Upto 50% </p>   <br>
+      <p id="g1text"> Lorem ipsum, dolor sit amet consectetur <br> adipisicing elit. Animi obcaecati voluptas <br> omnis
+        amet minima!</p>
+
+    </div>
+
+    <div class="group1btn">
+      <button id="g1btn" type="submit"> Order Now </button>
+    </div>
+
+  </div>
+
+
+   <div class="group2">
+    <div class="g2text">
+      <h1>Our Chef</h1> 
+    </div>
+            <div class="g2text2">
+              Lorem ipsum dolor sit amet consectetur , adipisicing elit. Laborum <br>  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  quia obcaecati  quam veniam error.
+   
+            </div>
+        
+            <div class="aid">
+               <img id="aidtext" src="/Aiden.png" alt="">
+               <h2>EIDEN HUNTER</h2> 
+                   Founder
+            </div>
+             <div class="eth">
+              <img src="/Ethel.png" alt="">
+              <h2>ETHEL RAMSEY</h2> <br> 
+                  Co-Founder
+                        </div>
+            <div class="fann">
+            <img src="/Fannie.png" alt="">
+            <h2>FANNIE STEWART</h2> <br> 
+              Co-Founder
+            </div>
+             
+            <div class="mus">
+            <img src="/Mushroom.png" alt="">
+            </div>
+            <div class="gar">
+             <img src="/Garlic.png" alt="">
+            </div>
+            <div class="fries">
+            <img src="/Fries.png" alt="">
+            </div>
+            <div class="leaf">
+            <img src="/Leaf.png" alt="">
+            </div>
+            <div class="bur">
+              <img src="/Burger.png" alt="">
+            </div>
+          <div class="chi">
+            <img src="/ChimlaMirch.png" alt="">
+          </div>
+      
+  </div> -->
   );
 }
 
